e3c80f2ba0ca004576329659dbba4bca0c40c304
LH <luhui1669@sina.com>
2016-07-21 15:17:54 +0800

    new project

---
+++ new/new/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  new
+//
+//  Created by LH on 16/7/21.
+//  Copyright © 2016年 Alesary Gru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ new/new/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  new
+//
+//  Created by LH on 16/7/21.
+//  Copyright © 2016年 Alesary Gru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
